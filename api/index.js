79186ebffe94e7db5868a03ad14a54026a7affe9
窪山天望 <shiggyjr1492@gmail.com>
2023-09-18 15:41:48 +0900

    Fix: vercelでホスティングできるように修正しました。

--- api/index.js
+++ api/index.js
@@ -9,30 +9,29 @@ app.use(express.static("./public"));
 
 app.get('/atcoder-data', async (req, res) => {
     try {
-        // クライアントからのリクエストを待つために非同期処理を使用
-        await new Promise((resolve) => setTimeout(resolve, 1000)); // 1秒待機 (必要に応じて調整)
-
-        const name = req.query.name; // クエリパラメータ name を取得
+        const name = req.query.name;
 
         if (!name) {
             throw new Error('Name is missing in the query');
         }
 
-        // クエリパラメータ name をURLに組み込んでfetchリクエストを送信
         const response = await fetch(`http://kyopro-ratings.jp1.su8.run/json?atcoder=${name}`);
+        
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
+
         const data = await response.json();
-        res.json(data); // クライアントにJSONデータを返す
+        res.json(data);
     } catch (error) {
         console.error('Fetch error:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
